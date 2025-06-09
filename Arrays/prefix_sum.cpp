93e12616dbbef241a5c914385644a497fc5ce960
Devyani2004 <135535177+Devyani2004@users.noreply.github.com>
2025-06-09 20:03:42 +0530

    Update prefix_sum.cpp

--- Arrays/prefix_sum.cpp
+++ Arrays/prefix_sum.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {0, 1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int prefix[n];
+    prefix[0] = arr[0];
+    for (int i = 1; i < n; ++i) {
+        prefix[i] = prefix[i-1] + arr[i];
+    }
+
+    cout << "Prefix sum array: ";
+    for (int i = 0; i < n; ++i) {
+        cout << prefix[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
 
