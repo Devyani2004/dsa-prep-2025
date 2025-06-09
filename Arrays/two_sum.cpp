563f8e362c6e953b13fe7846f76eba7b769bfae5
Devyani2004 <135535177+Devyani2004@users.noreply.github.com>
2025-06-09 19:22:40 +0530

    Update two_sum.cpp

--- Arrays/two_sum.cpp
+++ Arrays/two_sum.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {1, 90, 100, 99, 50};
+    int target = 51;
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(arr[i] + arr[j] == target){
+                cout << "true" << endl;
+                return 1;
+                break;
+            }
+        }
+    }
+
+    cout << "false" << endl;
+    return 0;
+}
 
