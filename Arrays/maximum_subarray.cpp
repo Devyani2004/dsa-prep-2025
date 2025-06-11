d51ed2ce78d6d064e070fdcfc56169deb1f2dfdc
Devyani2004 <135535177+Devyani2004@users.noreply.github.com>
2025-06-11 22:49:57 +0530

    Update maximum_subarray.cpp

--- Arrays/maximum_subarray.cpp
+++ Arrays/maximum_subarray.cpp
@@ -1,1 +1,13 @@
+int maxSubarraySum(vector<int> &arr) {
+    int res = arr[0];
+  
+  	for(int i = 0; i < arr.size(); i++) {
+    	int currSum = 0;
+        for(int j = i; j < arr.size(); j++) {
+        	currSum = currSum + arr[j];
+            res = max(res, currSum);
+        }
+    }
+    return res;
+}
 
