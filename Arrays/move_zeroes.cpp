31cd54731e6c704644ea1907e0ec8ce30e642448
Devyani2004 <135535177+Devyani2004@users.noreply.github.com>
2025-06-11 23:32:14 +0530

    Update move_zeroes.cpp

--- Arrays/move_zeroes.cpp
+++ Arrays/move_zeroes.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+vector<int> move_Zeros(vector <int> &arr){
+
+    int nzeroidx = 0;
+
+    for(int i=0; i<arr.size(); i++){
+       if(arr[i]!=0){
+        arr[nzeroidx++]= arr[i];
+       } 
+    }
+
+    for(int i= nzeroidx; i<arr.size(); i++){
+        arr[i] =0;
+    }
+
+    return arr;
+}
+
+int main(){
+    vector <int> arr= {0, 2, 02,2, 0, 0, 5, 6};
+    vector <int> result= move_Zeros(arr);
+
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
