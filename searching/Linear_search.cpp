941ef8cfbf77f230d93586a73781527b4803e2d7
Devyani2004 <135535177+Devyani2004@users.noreply.github.com>
2025-06-14 19:20:15 +0530

    Update Linear_search.cpp

--- searching/Linear_search.cpp
+++ searching/Linear_search.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int Linear_Search(vector <int> &arr, int target){
+    for(int i=0; i<arr.size(); i++){
+        if(arr[i]==target){
+            return 1;
+        }
+    }
+
+    return 0;
+
+
+}
+
+int main(){
+    vector <int> arr = {1, 2, 3, 44, 89};
+    int target = 89;
+
+    cout<< Linear_Search(arr, target) <<endl;
+}
