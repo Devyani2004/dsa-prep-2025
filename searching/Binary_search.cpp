819c398aa984fdcb05557fcadb452d3b9b169c03
Devyani2004 <135535177+Devyani2004@users.noreply.github.com>
2025-06-14 19:30:50 +0530

    Update Binary_search.cpp

--- searching/Binary_search.cpp
+++ searching/Binary_search.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int Binary_search(vector<int> arr, int target){
+     int n = arr.size();
 
+    int left=0;
+    int right= n-1;
+
+    while(left<=right){
+
+        int mid=(left+right)/2;
+
+        if(arr[mid]== target){
+            return mid;
+        }
+
+        else if (arr[mid]<target)
+        {
+            
+            left= mid+1;
+        }
+
+        else{
+            right= mid-1;
+        }
+        
+    }
+
+    return -1;
+}
+
+
+int main(){
+    vector <int> arr= {1, 2, 3,4 , 5,6 ,7, 9};
+
+    int target= 9;
+
+    cout<<Binary_search(arr, target) <<endl;
+}
