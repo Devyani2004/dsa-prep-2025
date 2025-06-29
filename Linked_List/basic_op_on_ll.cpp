a87ddd0ef77461c2de400c2e89229d013f6ded31
Devyani2004 <135535177+Devyani2004@users.noreply.github.com>
2025-06-29 22:05:12 +0530

    Update basic_op_on_ll.cpp

--- Linked_List/basic_op_on_ll.cpp
+++ Linked_List/basic_op_on_ll.cpp
@@ -1,1 +1,62 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+class Node{
+
+public:
+    int data; // the data part
+
+    Node * next; // ptr to link to the next node
+
+    Node(int new_data){
+        this->data= new_data; //cont that'll initialize the node with data
+        this->next= nullptr;
+    }
+};
+
+//traversal
+
+void traverse(Node * head){
+    while(head!= nullptr){
+        cout<<head->data<<" ";
+
+        head= head->next;
+
+    }
+
+    cout<<endl;
+
+}
+
+
+
+
+//searching
+
+void search_in_linked_list(Node * head, int n){
+    while(head!= nullptr){
+        if(head->data==n){
+            cout<<"The element is present"<<endl;
+
+            return;
+        }
+        head = head->next; 
+    }
+
+    cout<<"element not found"<<endl;
+}
+
+
+int main(){
+    Node* head= new Node(1);
+    head->next= new Node(2);
+    head->next->next = new Node(6);
+    head->next->next->next = new Node(8);
+
+    traverse(head);
+    search_in_linked_list(head, 10);
+
+    return 0;
+}
